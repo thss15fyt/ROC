561711dcf56b7d331a7049950d37591cbda8c484
thss15fyt <thss15_fengyt@163.com>
2017-12-03 16:12:00 +0800

    Add status for all models. Ref #1

--- ROC/models.py
+++ ROC/models.py
@@ -8,22 +8,33 @@ class CoursePost(models.Model):
     name = models.CharField(max_length=128)
     teacher = models.CharField(max_length=20)
     apartment = models.CharField(max_length=128)
+    status = models.IntegerField(default=1)
+    PUBLISHED = 1
+    DELETED = -1
 
 
 class CourseComment(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User)
     create_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(default=1)
+    PUBLISHED = 1
+    DELETED = -1
 
 
 class ExchangePost(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
     author = models.ForeignKey(User)
+    status = models.IntegerField(default=1)
+    PUBLISHED = 1
+    DELETED = -1
 
 
 class ExchangeComment(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User)
     create_at = models.DateTimeField(auto_now_add=True)
-
+    status = models.IntegerField(default=1)
+    PUBLISHED = 1
+    DELETED = -1
