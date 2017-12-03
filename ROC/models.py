85f481dfffb9f089f81de30034c0eda5b5def285
thss15fyt <thss15_fengyt@163.com>
2017-12-03 14:41:23 +0800

    Implement database design. Ref #1.

--- ROC/models.py
+++ ROC/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class CoursePost(models.Model):
+    name = models.CharField(max_length=128)
+    teacher = models.CharField(max_length=20)
+    apartment = models.CharField(max_length=128)
+
+
+class CourseComment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+class ExchangePost(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+
+
+class ExchangeComment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User)
+    create_at = models.DateTimeField(auto_now_add=True)
+
