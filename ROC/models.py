38a2b3bb07655a7bbeceed83b78aca087a05b466
thss15fyt <thss15_fengyt@163.com>
2017-12-06 20:07:13 +0800

    Modify database design for more course info

--- ROC/models.py
+++ ROC/models.py
@@ -4,34 +4,39 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class CoursePost(models.Model):
+class Apartment(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Course(models.Model):
     name = models.CharField(max_length=128)
     teacher = models.CharField(max_length=20)
-    apartment = models.CharField(max_length=128)
+    apartment = models.ForeignKey(Apartment)
+    course_id = models.CharField(max_length=32)
+    credit = models.PositiveIntegerField()
+    undergraduate_capacity = models.PositiveIntegerField()
+    postgraduate_capacity = models.PositiveIntegerField()
+    explain = models.CharField(max_length=256)
+    feature = models.CharField(max_length=256)  # TODO: split feature into models
+    grade = models.CharField(max_length=64)
+    second_choose_flag = models.BooleanField()  # 是否二级选课
+    retake_flag = models.BooleanField()     # 重修是否占容量
+    choose_restrict_flag = models.BooleanField()    # 是否选课时限制
+    group = models.CharField(max_length=64)     # 本科文化素质课组
+
     status = models.IntegerField(default=1)
     PUBLISHED = 1
     DELETED = -1
+
+
+class Class(models.Model):
+    course = models.ForeignKey(Course)
+    class_id = models.CharField(max_length=32)
+    time = models.CharField(max_length=256)  # TODO: split class time into models
 
 
 class CourseComment(models.Model):
-    content = models.TextField()
-    author = models.ForeignKey(User)
-    create_at = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(default=1)
-    PUBLISHED = 1
-    DELETED = -1
-
-
-class ExchangePost(models.Model):
-    title = models.CharField(max_length=256)
-    content = models.TextField()
-    author = models.ForeignKey(User)
-    status = models.IntegerField(default=1)
-    PUBLISHED = 1
-    DELETED = -1
-
-
-class ExchangeComment(models.Model):
+    course = models.ForeignKey(Course)
     content = models.TextField()
     author = models.ForeignKey(User)
     create_at = models.DateTimeField(auto_now_add=True)
